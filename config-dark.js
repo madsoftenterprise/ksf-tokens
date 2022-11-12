4bb30fb1da442e0871aa52d655e27ea79180f434
kosov <kosov@kraud.ru>
2022-11-12 23:18:23 +0600

    add tokens

---
+++ config-dark.js
@@ -0,0 +1,43 @@
+module.exports = {
+  format: {
+    customFormat: function({dictionary, options}) {
+      return `:[data-theme=dark]{\n
+      ${dictionary.allTokens.map(token => {        
+        if (token.name.includes('font-weight')) {
+          if (!token.name.indexOf('f')) {
+            return `--${token.name}: ${token.description};`
+          }
+          const fontWeight = dictionary
+            .allTokens.find(item => {
+              return item.type === 'fontWeights' && item.value === token.value
+            }).description
+
+          return `--${token.name}: ${fontWeight};`
+        }
+        return `--${token.name}: ${token.value};`
+      }).join(`\n`)}\n } `
+    }
+  },
+
+  source: ['tokens/**/*.json'],
+  platforms: {
+
+    "css": {
+      "transformGroup": "css",
+      "buildPath": "./css/",
+      "files": [{
+        "destination": "variables-dark.css",
+        "format": "customFormat",
+        "options": {
+          "outputReferences": true
+        }
+      },{
+        "destination": "variables-dark.scss",
+        "format": "scss/variables",
+        "options": {
+          "outputReferences": true
+        }
+      }]
+    }
+  }
+};
